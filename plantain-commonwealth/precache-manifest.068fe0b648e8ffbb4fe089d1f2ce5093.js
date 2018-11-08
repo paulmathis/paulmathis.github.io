84286388b2301d2d738aa018dbb57455559c8bf6
Paul Mathis <paul.mathis@gmail.com>
2018-11-08 13:28:14 -0600

    plantain-commonwealth added

---
+++ plantain-commonwealth/precache-manifest.068fe0b648e8ffbb4fe089d1f2ce5093.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3c16573c672bc60aebf628354503b77",
+    "url": "/static/media/jacket.a3c16573.jpg"
+  },
+  {
+    "revision": "c13f78319d9245a083ee",
+    "url": "/static/css/main.848ed9b0.chunk.css"
+  },
+  {
+    "revision": "36dfe4071ba57986079695efd2a258f9",
+    "url": "/static/media/hero-men.36dfe407.jpg"
+  },
+  {
+    "revision": "0fc7ee5b5e065932dc3c",
+    "url": "/static/js/1.0fc7ee5b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca8701fd5e3b960617dbde9ff87c9f97",
+    "url": "/static/media/green-t-shirt.ca8701fd.jpg"
+  },
+  {
+    "revision": "6fcf0754f7c469e4a30faff388c4e599",
+    "url": "/static/media/logo.6fcf0754.png"
+  },
+  {
+    "revision": "890eace47c3cef57567e8b67ac1b3000",
+    "url": "/static/media/shirt.890eace4.jpg"
+  },
+  {
+    "revision": "5c14e7b86c0cf721835062773cabe250",
+    "url": "/static/media/shoes.5c14e7b8.jpg"
+  },
+  {
+    "revision": "c13f78319d9245a083ee",
+    "url": "/static/js/main.c13f7831.chunk.js"
+  },
+  {
+    "revision": "2a1a991955f00766604d01e0ca0123ba",
+    "url": "/static/media/dress.2a1a9919.jpg"
+  },
+  {
+    "revision": "dcab40c7f4d586c985602bdba0de65e4",
+    "url": "/static/media/watches.dcab40c7.jpg"
+  },
+  {
+    "revision": "078e4b5de4df628a35823dd49dbb972f",
+    "url": "/static/media/bag.078e4b5d.jpg"
+  },
+  {
+    "revision": "d3e99ff37bbc031bec4cb65e65f37d79",
+    "url": "/static/media/grey-t-shirt.d3e99ff3.jpg"
+  },
+  {
+    "revision": "bfd10407e59edece2c760282d1e81190",
+    "url": "/static/media/blue-t-shirt.bfd10407.jpg"
+  },
+  {
+    "revision": "ba8fdca7fda7971902b5f8c3c0fc5454",
+    "url": "/static/media/purple-t-shirt.ba8fdca7.jpg"
+  },
+  {
+    "revision": "5327f784bb0680e5eec3ba765509549e",
+    "url": "/static/media/hero-home.5327f784.jpg"
+  },
+  {
+    "revision": "0fc7ee5b5e065932dc3c",
+    "url": "/static/css/1.cfc2129d.chunk.css"
+  },
+  {
+    "revision": "eac773628e0e159e41213a01ff39ea1d",
+    "url": "/index.html"
+  }
+];
