23be364c1b60cadece52ea64dc62593d61484d6b
Paul Mathis <paul.mathis@gmail.com>
2018-11-08 13:47:25 -0600

    Updated pathing for pc

---
+++ plantain-commonwealth/precache-manifest.b14f18d2364e2b2b005a8a1e02aebf6c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3c16573c672bc60aebf628354503b77",
+    "url": "/plantain-commonwealth/static/media/jacket.a3c16573.jpg"
+  },
+  {
+    "revision": "fcc1ca389c9b2740f83e",
+    "url": "/plantain-commonwealth/static/css/main.848ed9b0.chunk.css"
+  },
+  {
+    "revision": "36dfe4071ba57986079695efd2a258f9",
+    "url": "/plantain-commonwealth/static/media/hero-men.36dfe407.jpg"
+  },
+  {
+    "revision": "73a667c540d81e0c0be6",
+    "url": "/plantain-commonwealth/static/js/1.73a667c5.chunk.js"
+  },
+  {
+    "revision": "ae58c5dc602c11851942",
+    "url": "/plantain-commonwealth/static/js/runtime~main.ae58c5dc.js"
+  },
+  {
+    "revision": "ca8701fd5e3b960617dbde9ff87c9f97",
+    "url": "/plantain-commonwealth/static/media/green-t-shirt.ca8701fd.jpg"
+  },
+  {
+    "revision": "6fcf0754f7c469e4a30faff388c4e599",
+    "url": "/plantain-commonwealth/static/media/logo.6fcf0754.png"
+  },
+  {
+    "revision": "890eace47c3cef57567e8b67ac1b3000",
+    "url": "/plantain-commonwealth/static/media/shirt.890eace4.jpg"
+  },
+  {
+    "revision": "5c14e7b86c0cf721835062773cabe250",
+    "url": "/plantain-commonwealth/static/media/shoes.5c14e7b8.jpg"
+  },
+  {
+    "revision": "fcc1ca389c9b2740f83e",
+    "url": "/plantain-commonwealth/static/js/main.fcc1ca38.chunk.js"
+  },
+  {
+    "revision": "2a1a991955f00766604d01e0ca0123ba",
+    "url": "/plantain-commonwealth/static/media/dress.2a1a9919.jpg"
+  },
+  {
+    "revision": "dcab40c7f4d586c985602bdba0de65e4",
+    "url": "/plantain-commonwealth/static/media/watches.dcab40c7.jpg"
+  },
+  {
+    "revision": "078e4b5de4df628a35823dd49dbb972f",
+    "url": "/plantain-commonwealth/static/media/bag.078e4b5d.jpg"
+  },
+  {
+    "revision": "d3e99ff37bbc031bec4cb65e65f37d79",
+    "url": "/plantain-commonwealth/static/media/grey-t-shirt.d3e99ff3.jpg"
+  },
+  {
+    "revision": "bfd10407e59edece2c760282d1e81190",
+    "url": "/plantain-commonwealth/static/media/blue-t-shirt.bfd10407.jpg"
+  },
+  {
+    "revision": "ba8fdca7fda7971902b5f8c3c0fc5454",
+    "url": "/plantain-commonwealth/static/media/purple-t-shirt.ba8fdca7.jpg"
+  },
+  {
+    "revision": "5327f784bb0680e5eec3ba765509549e",
+    "url": "/plantain-commonwealth/static/media/hero-home.5327f784.jpg"
+  },
+  {
+    "revision": "73a667c540d81e0c0be6",
+    "url": "/plantain-commonwealth/static/css/1.cfc2129d.chunk.css"
+  },
+  {
+    "revision": "cbc4696c3436583617fab65cc094ae52",
+    "url": "/plantain-commonwealth/index.html"
+  }
+];
--- plantain-commonwealth/service-worker.js
+++ plantain-commonwealth/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.068fe0b648e8ffbb4fe089d1f2ce5093.js"
+  "/plantain-commonwealth/precache-manifest.b14f18d2364e2b2b005a8a1e02aebf6c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/plantain-commonwealth/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
